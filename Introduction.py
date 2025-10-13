492e80765928eea4f026702264e270c6d9ef8ca4
GlennUbly <88552377+GlennUbly@users.noreply.github.com>
2025-10-13 23:43:42 +0100

    Add files via upload
    Initial upload 13/10/2025

---
+++ Introduction.py
@@ -0,0 +1,39 @@
+# Streamlit app for PPCI travel times in Kent and Medway
+# 13/10/2025 GU
+# Page 0 for intro
+# Updated 13/10/2025 for use of 24/25 base data
+
+import streamlit as st
+
+st.title('Reducing Travel Times to Treatment for PPCI Patients in Kent and Medway ICB - 2024/25 update')
+
+st.markdown('This application is based on work originally created as as part of the [HSMA4 programme](https://sites.google.com/nihr.ac.uk/hsma).')
+
+st.markdown("#### A comparison of possible configurations of sites offering "+
+            "this service, looking at the potential impact on travel times "+
+            "and distances")
+
+st.markdown('This is an update of earlier work based on the [2022/23 activity](https://kmppci.streamlit.app/) ')
+
+#st.markdown('Note that for this update, the travel times are taken from an NCDR reference table which typically shows greater '+
+#            'distances and times than we saw using Routino for the 2022/23 data ')
+
+with st.expander('Resources used:',
+                 expanded=False) :
+    st.markdown('This is app is created and shared using using [Streamlit](https://streamlit.io/) ')
+    st.markdown('Time and distance calculations from [Routino](https://www.routino.org/uk/) ')
+    st.markdown('NHS activity data from [National Commissioning Data Repository](https://www.ardengemcsu.nhs.uk/services/business-intelligence/ncdr/) ')
+    st.markdown('LSOA geographic data from [Cambridgeshire Insight Open Data](https://data.cambridgeshireinsight.org.uk/dataset/output-areas)')
+    st.markdown('NHS ICB geographic data from [Office for National Statistics](https://geoportal.statistics.gov.uk/maps/d6acd30ad71f4e14b4de808e58d9bc4c)')
+    st.markdown('##### Python dependencies:')
+    st.text("""
+            bokeh==2.4.3
+            geopandas==0.9.0
+            matplotlib==3.5.1
+            numpy==1.22.3
+            pandas<2.0
+            Pillow==9.2.0
+            scipy==1.6.2
+            seaborn==0.11.2
+            streamlit==1.11.1
+            """)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-An update on eadlier work on the potential sites in the Kent and Medway area for a new cardiac PPCI serviec, looking at the impact on patient travel times and distances
+# km_ppci
